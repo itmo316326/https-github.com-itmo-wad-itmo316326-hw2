6dfcec5feca19a703ac0be1eef0283616589c8bb
itmo316326 <91429894+itmo316326@users.noreply.github.com>
2022-03-25 14:55:26 +0300

    Update login.py

--- login.py
+++ login.py
@@ -1,23 +1,55 @@
-from flask import Flask,request,render_template,redirect, session
-import pymongo
+from flask import Flask, request, render_template, redirect, session, url_for
+from pymongo import MongoClient
 
 app = Flask(__name__)
-@app.route("/login",methods=['GET','POST'])
+
+client = MongoClient('localhost', 27017)
+db = client.wad
+
+
+@app.route("/", methods=['GET', 'POST'])
 def login():
-    if request.method =='POST':
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        client = pymongo.MongoClient(host='localhost', port=27017)
-        db = client.mydb
-        collection = db.user
-        useronline = collection.find({"username":username,"password":password}).count()
-        if useronline:
+        is_user_online = db.users.find_one({"username": username, "password": password})
+        if is_user_online:
             session['username'] = username
-            return redirect(" http://localhost:5000/profile")
+            return redirect(url_for("http://localhost:5000/profile"))
         else:
             message = "Failed Login"
-            return render_template('login.html',message=message)
+            return render_template('login.html', message=message)
     return render_template('login.html')
 
+
+@app.route("/registered/", methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user_is_online = db.insert_one({"username": username, "password": password})
+        if user_is_online:
+            session['username'] = username
+            return redirect(url_for("http://localhost:5000/profile"))
+        else:
+            message = 'User not registered'
+            return render_template('register.html', message=message)
+    return render_template('register.html')
+
+
+@app.route("/profile")
+def profile():
+    if session.get("username"):
+        return redirect(url_for("http://localhost:5000/profile/"))
+    else:
+        return render_template('login.html')
+    return render_template('profile.html')
+
+
+def log_out():
+    session.clear()
+    return redirect('login.html')
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='localhost', port=5001, debug=True)
